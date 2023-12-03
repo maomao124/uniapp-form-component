0f02133d7070249be72a38c3dff9cb3d6b93411d
mao <1296193245@qq.com>
2023-12-03 12:49:25 +0800

    uniapp-form-component

---
+++ pages/index/test2/test2.vue
@@ -0,0 +1,41 @@
+<template>
+	<view>
+		<checkbox-group @change="c">
+			<checkbox value="1">内容1</checkbox>
+			<checkbox value="2">内容2</checkbox>
+			<checkbox value="3">内容3</checkbox>
+			<checkbox value="4">内容4</checkbox>
+		</checkbox-group>
+		<checkbox-group>
+			<checkbox value="1">内容1</checkbox>
+			<checkbox value="2" checked="true">内容2</checkbox>
+			<checkbox value="3" checked="true">内容3</checkbox>
+		</checkbox-group>
+		<checkbox-group>
+			<checkbox value="1">内容1</checkbox>
+			<checkbox value="2" checked="true" disabled="true">内容2</checkbox>
+			<checkbox value="3" disabled="true">内容3</checkbox>
+		</checkbox-group>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			c(e)
+			{
+				console.log(e);
+				console.log(e.target.value);
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/test3/test3.vue
@@ -0,0 +1,50 @@
+<template>
+	<view>
+		<view class="container">
+			<editor id="editor" class="ql-container" :placeholder="placeholder" @ready="onEditorReady"></editor>
+			<button type="warn" @tap="undo">撤销</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				placeholder: '开始输入...'
+			}
+		},
+		methods: {
+			onEditorReady() {
+				// #ifdef MP-BAIDU
+				this.editorCtx = requireDynamicLib('editorLib').createEditorContext('editor');
+				// #endif
+
+				// #ifdef APP-PLUS || H5 ||MP-WEIXIN
+				uni.createSelectorQuery().select('#editor').context((res) => {
+					this.editorCtx = res.context
+				}).exec()
+				// #endif
+			},
+			undo() {
+				this.editorCtx.undo()
+			}
+		}
+	}
+</script>
+
+<style>
+.container {
+		padding: 10px;
+	}
+
+	#editor {
+		width: 100%;
+		height: 300px;
+		background-color: #CCCCCC;
+	}
+
+	button {
+		margin-top: 10px;
+	}
+</style>
