f3b320130215dc6cf7498350198d35effb4db8f7
mao <1296193245@qq.com>
2023-12-06 14:33:24 +0800

    uniapp-form-component

---
+++ pages/index/test15/test15.vue
@@ -0,0 +1,39 @@
+<template>
+	<view>
+		<view>
+			<switch></switch>
+		</view>
+		<view>
+			<switch checked="true"></switch>
+		</view>
+		<view>
+			<switch checked="true" disabled="true"></switch>
+		</view>
+		<view>
+			<switch checked="true" color="#ffaaaa"></switch>
+		</view>
+		<view>
+			<switch checked="true" type="checkbox" color="#ffaaaa"></switch>
+		</view>
+		<view>
+			<switch checked="true" color="#ffaaaa" style="transform:scale(2)"></switch>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/test16/test16.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+		<view>
+			<textarea @input="change"></textarea>
+		</view>
+		<view>
+			<textarea @input="change" auto-height="true"></textarea>
+		</view>
+		<view>
+			<textarea @input="change" maxlength="9999" auto-height="true"></textarea>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			change(e)
+			{
+				console.log("长度：",e.detail.value.length,'值:',e.detail.value);
+			}
+		}
+	}
+</script>
+
+<style>
+textarea{
+	background-color: bisque;
+	margin: 2ch;
+}
+</style>
