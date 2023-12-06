4409cfb7b4b2acd3213d78ea8f51466403dba4fb
mao <1296193245@qq.com>
2023-12-06 12:05:47 +0800

    uniapp-form-component

---
+++ pages/index/test13/test13.vue
@@ -0,0 +1,45 @@
+<template>
+	<view>
+		<radio-group @change="change">
+			<radio value="1">单选框1</radio>
+			<radio value="2">单选框2</radio>
+			<radio value="3">单选框3</radio>
+		</radio-group>
+		<radio-group @change="change">
+			<radio value="1">单选框1</radio>
+			<radio value="2">单选框2</radio>
+			<radio value="3" disabled="true">单选框3</radio>
+		</radio-group>
+		<radio-group @change="change">
+			<radio value="1" checked="true">单选框1</radio>
+			<radio value="2">单选框2</radio>
+			<radio value="3">单选框3</radio>
+		</radio-group>
+		<radio-group @change="change">
+			<radio value="1" color="#aaff00">单选框1</radio>
+			<radio value="2" color="#aaff00">单选框2</radio>
+			<radio value="3" color="#aaff00">单选框3</radio>
+		</radio-group>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			change(e)
+			{
+				console.log(e);
+				console.log("当前选择的是：",e.detail.value);
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/test14/test14.vue
@@ -0,0 +1,52 @@
+<template>
+	<view>
+		<view>
+			<slider value="60" @change="sliderChange" />
+		</view>
+		<view>
+			<slider value="60" @change="sliderChange" show-value />
+		</view>
+		<view>
+			<slider value="240" min="100" max="3000" @change="sliderChange" show-value />
+		</view>
+		<view>
+			<slider value="60" step="10" @change="sliderChange" show-value/>
+		</view>
+		<view>
+			<slider value="44" disabled="true" @change="sliderChange" show-value />
+		</view>
+		<view>
+			<slider value="60" @change="sliderChange" active-color="red" background-color="green" show-value />
+		</view>
+		<view>
+			<slider value="72" @change="sliderChange" 
+			active-color="red" background-color="green"
+			block-color="skyblue" show-value />
+		</view>
+		<view>
+			<slider value="45" @change="sliderChange" 
+			active-color="red" background-color="green"
+			block-color="skyblue" block-size="12" show-value />
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			sliderChange(e)
+			{
+				console.log(e.detail.value);
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
